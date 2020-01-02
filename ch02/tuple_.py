782fc238da4a664eea509bc5ee213c705ac91568
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-01-02 17:55:23 +0800

    Updated ch02/tuple_.py

--- ch02/tuple_.py
+++ ch02/tuple_.py
@@ -5,12 +5,18 @@
 Traceback (most recent call last):
 ...
 TypeError: 'tuple' object does not support item assignment
+
+
+# 元组使用小括号语法，但要与普通括号区别开来。
 >>> a = (1, )  # 仅一个元素的元组
 >>> type(a)
 <class 'tuple'>
 >>> b = (1)
 >>> type(b)  # 普通括号
 <class 'int'>
+
+
+# 支持与列表类似的运算符操作。但不能修改，总是返回新对象
 >>> (1, 2) + (3, 4)
 (1, 2, 3, 4)
 >>> (1, 2) * 2
@@ -22,6 +28,10 @@ TypeError: 'tuple' object does not support item assignment
 (1, 2, 3, 4, 5)
 >>> b
 (1, 2, 3)
+
+
+# 因列表支持插入、删除等修改操作，所以序号无法与元素对象构成固定映射。
+# 但元组不同，相同序号总是返回同一对象，故可为序号取个“别名”。
 >>> import collections
 >>> User = collections.namedtuple('User', 'name, age')  # 创建 User 类型，指定字段
 >>> issubclass(User, tuple)  # tuple子类
