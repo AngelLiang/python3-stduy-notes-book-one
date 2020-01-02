7951d8bb8595390ed2167c7641d298ab04a2b018
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-01-02 15:33:00 +0800

    Updated string_.py

--- ch02/string_.py
+++ ch02/string_.py
@@ -25,6 +25,7 @@
 ... \"""
 '\\nBeautiful is batter than ugly.\\nExplicit is better than implicit.\\nSimple is better than complex.\\n'
 
+
 # 在字面量前添加标志，指示构建特定格式字符串。
 >>> f = open(r'.\README.md')  # Windows路径
 >>> import re
@@ -35,6 +36,7 @@
 >>> type(b'abc')  #构建字节数组
 <class 'bytes'>
 
+
 # 操作
 # 支持用加法或乘法运算符拼接字符串。
 >>> s = 'hello'
@@ -42,11 +44,12 @@
 >>> '-' * 10
 '----------'
 
-# 多个动态字符串拼接，应优先选择join或format方式。
+
+# 多个动态字符串拼接，应优先选择 join 或 format 方式。
 # 相比于多次加法运算和多次内存分配（字符串是不可变对象），
-# join这类函数（方法）可预先计算出总长度，一次性分配内存，
+# join 这类函数（方法）可预先计算出总长度，一次性分配内存，
 # 随后直接复制内存数据填充。
-# 另一方面，将固定模板内容与变量分离的format，更易阅读和维护。
+# 另一方面，将固定模板内容与变量分离的 format ，更易阅读和维护。
 >>> username = 'qyuhen'
 >>> datetime = '20170101'
 >>> '/data/' + username + '/message/' + datetime + '.txt'  # 糟糕的方式
@@ -54,6 +57,64 @@
 >>> tmpl = '/data/{user}/message/{time}.txt'  # 模板单独维护
 >>> tmpl.format(user=username, time=datetime)  # 无须考虑参数的次序
 '/data/qyuhen/message/20170101.txt'
+
+
+>>> 'py' in 'python'
+True
+>>> 'Py' not in 'python'
+True
+
+
+# 作为序列类型，可使用索引序号访问字符串的单个字符或者某一片段。
+>>> s = '0123456789'
+>>> s[2]
+'2'
+>>> s[-1]
+'9'
+>>> s[2:6]
+'2345'
+>>> s[2:-2]
+'234567'
+
+
+# 使用两个索引号表示一个序列片段的语法被称作切片（slice），可以此返回字符串子串。
+# 无论以哪种方式返回与原字符串内容不同的子串时，都可能会重新分配内存，并复制数据。
+>>> s = '-' * 1024
+>>> s1 = s[10:100]  # 片段，内容不同
+>>> s2 = s[:]  # 内容相同
+>>> s3 = s.split(',')[0]  # 内容相同
+>>> s1 is s  # 内容不同，构建新对象
+False
+>>> s2 is s  # 内容相同时，直接引用原字符串对象
+True
+>>> s3 is s
+True
+
+
+# 转换
+>>> s = '汉字'
+>>> b = s.encode('utf-16')  # to bytes
+>>> b.decode('utf-16')  # to Unicode str
+'汉字'
+
+
+# 如要处理BOM信息，可导入 codecs 模块。
+>>> import codecs
+>>> s = '汉字'
+>>> s.encode('utf-8').hex()
+'e6b189e5ad97'
+>>> codecs.BOM_UTF16_LE.hex()  # BOM标志
+'fffe'
+>>> codecs.encode(s, 'utf-16be').hex()  # 按指定BOM转换
+'6c495b57'
+>>> codecs.encode(s, 'utf-16le').hex()
+'496c575b'
+
+
+# Python 3默认编码不再是ASCII，无须额外设置。
+>>> import sys
+>>> sys.getdefaultencoding()
+'utf-8'
 """
 
 
