8627a2797d22b0b3529f173fc6a175d0f6d7b2fe
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-01-03 09:48:09 +0800

    Updated ch02/integer.py

--- ch02/integer.py
+++ ch02/integer.py
@@ -7,15 +7,16 @@
 >>> sys.getsizeof(x)
 28
 >>> y = 1 << 10000
->>> # y  # skip
-...
+>>> y
+199506311688075838488...792596709376
 >>> type(y)
 <class 'int'>
 >>> sys.getsizeof(y)
 1360
 
-# 对于较长数字，为便于阅读，习惯以千分位进行分隔标记。
-# 但逗号在Python语法中有特殊含义，故改用下画线表示，且不限分隔位数。
+
+对于较长数字，为便于阅读，习惯以千分位进行分隔标记。
+但逗号在Python语法中有特殊含义，故改用下画线表示，且不限分隔位数。
 >>> 78,654,321
 (78, 654, 321)
 >>> 78_654_321
@@ -28,6 +29,8 @@
 100
 >>> 0b_11001_1
 51
+
+
 
 # 转换
 >>> bin(100)
@@ -49,7 +52,7 @@
 >>> int('\t100\t')
 100
 
-# 用 eval 也能完成转换，性能较差
+## 用 eval 也能完成转换，性能较差
 >>> eval('0o114')
 76
 
@@ -71,13 +74,17 @@
 >>> 3 // 2  # floor divsion
 1
 
-# 如要获取余数，可用取模运算符（mod）或divmod函数。
+
+
+## 如要获取余数，可用取模运算符（mod）或 divmod 函数。
 >>> 5 % 2
 1
 >>> divmod(5, 2)
 (2, 1)
 
-# Python 3不再支持数字和非数字类型的比较操作
+
+
+## Python 3不再支持数字和非数字类型的比较操作
 >>> 1 > ''
 Traceback (most recent call last):
     ...
@@ -88,6 +95,8 @@ Traceback (most recent call last):
 TypeError: '<' not supported between instances of 'int' and 'list'
 """
 
+
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    optionflags = doctest.ELLIPSIS
+    doctest.testmod(optionflags=optionflags)
